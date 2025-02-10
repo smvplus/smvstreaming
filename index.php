51ab519821cca08c1697fe11c2d76494b08c24ce
smvplus <smartmovieplus@gmail.com>
2025-02-10 12:49:08 +0800

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,2 +0,0 @@
-<iframe name="iframe" src="<?php echo $_GET['id'] ?>" scrolling="no" frameborder="0"  allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" id="player-iframe" style="width: 100%;height: 100vh;margin: 0 auto" sandbox="allow-scripts allow-same-origin"></iframe>
-
